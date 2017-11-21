3bdf04b3f8f1892b598f6643587147278f245321
Alexander Kulba <tunet000@gmail.com>
2017-11-21 16:00:31 +0300

    apache2 settings

--- 03-configure-vars.php
+++ 03-configure-vars.php
@@ -2,5 +2,9 @@
 
 <?php
 
+$apacheConfPath = __DIR__ . '/dotfiles/apache2.conf';
+
 $config = parse_ini_file(__DIR__ . '/config.cfg');
-var_dump($config);
+$apacheConf = file_get_contents($apacheConfPath);
+$apacheConf = str_replace('{{--MY_DOMAINS_DIR--}}', $config['MY_DOMAINS_DIR'], $apacheConf);
+file_put_contents($apacheConfPath, $apacheConf);
--- 04-configure-dotfiles.sh
+++ 04-configure-dotfiles.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+source ./config.cfg
+
 # MySQL settings
 sudo rm /etc/mysql/mysql.conf.d/mysqld.cnf;
-sudo ln -s $HOME/dotfiles/dotfiles/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf;
-sudo sh -c "echo '\n\n@{HOMEDIRS}/** rwk,' >> /etc/apparmor.d/local/usr.sbin.mysqld";
+sudo ln -s $MY_DOTFILES_DIR/dotfiles/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf;
+sudo rm /etc/apparmor.d/local/usr.sbin.mysqld;
+sudo ln -s $MY_DOTFILES_DIR/dotfiles/apparmor-usr.sbin.mysqld /etc/apparmor.d/local/usr.sbin.mysqld;
 sudo service apparmor restart;
 sudo service mysql restart;
 
 # NGINX change port 80 -> 8085
 sudo rm /etc/nginx/sites-available/default;
-sudo ln -s $HOME/dotfiles/dotfiles/nginx-default /etc/nginx/sites-available/default;
+sudo ln -s $MY_DOTFILES_DIR/dotfiles/nginx-default /etc/nginx/sites-available/default;
 sudo service nginx restart;
 sudo service apache2 restart;
 
 # APACHE2 settings
-
+sudo rm /etc/apache2/sites-available/000-default.conf;
+sudo ln -s $MY_DOTFILES_DIR/dotfiles/apache2.conf /etc/apache2/sites-available/000-default.conf;
+sudo a2enmod rewrite;
+sudo service apache2 restart;
