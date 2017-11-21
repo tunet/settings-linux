42873a782a87f1dc8b7c8ce0e6f38e8d16cc6fab
Alexander Kulba <tunet000@gmail.com>
2017-11-21 12:20:25 +0300

    fix path

--- 03-configure-dotfiles.sh
+++ 03-configure-dotfiles.sh
@@ -2,13 +2,13 @@
 
 # MySQL settings
 sudo rm /etc/mysql/mysql.conf.d/mysqld.cnf;
-sudo ln -s $HOME/dotfiles/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf;
+sudo ln -s $HOME/dotfiles/dotfiles/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf;
 sudo sh -c "echo '\n\n@{HOMEDIRS}/** rwk,' >> /etc/apparmor.d/local/usr.sbin.mysqld";
 sudo service apparmor restart;
 sudo service mysql restart;
 
 # NGINX change port 80 -> 8085
 sudo rm /etc/nginx/sites-avaliable/default;
-sudo ln -s $HOME/dotfiles/nginx-default /etc/nginx/sites-avaliable/default;
+sudo ln -s $HOME/dotfiles/dotfiles/nginx-default /etc/nginx/sites-avaliable/default;
 sudo service nginx restart;
 sudo service apache2 restart;
