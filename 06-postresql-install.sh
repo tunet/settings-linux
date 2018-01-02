93c04e748aafdcd7b1b6b245993002c2260bf386
Alexander Kulba <tunet000@gmail.com>
2018-01-02 20:53:08 +0300

    fix path to pgdg.list

--- 06-postresql-install.sh
+++ 06-postresql-install.sh
@@ -2,7 +2,7 @@
 
 source ./config.cfg
 
-sudo cp $MY_DOTFILES_DIR/pgdg.list /etc/apt/sources.list.d/pgdg.list
+sudo cp $MY_DOTFILES_DIR/dotfiles/pgdg.list /etc/apt/sources.list.d/pgdg.list
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
   sudo apt-key add -
