6862a51715a83199c130de5e29dc467985c8aeba
tunet <tunet000@gmail.com>
2018-01-23 00:23:16 +0300

    fix compoer and phing install

--- 05-install-programs.sh
+++ 05-install-programs.sh
@@ -5,9 +5,9 @@ source ./config.cfg
 mkdir $HOME/bin;
 
 # COMPOSER
-ln -s $MY_DOTFILES_DIR/programs/composer.phar $HOME/bin/composer;
+ln -s $MY_DOTFILES_DIR/programs/composer.phar /usr/bin/composer;
 chmod 0777 $HOME/bin/composer;
 
 # PHING
-ln -s $MY_DOTFILES_DIR/programs/phing.phar $HOME/bin/phing;
+ln -s $MY_DOTFILES_DIR/programs/phing.phar /usr/bin/phing;
 chmod 0777 $HOME/bin/phing;
